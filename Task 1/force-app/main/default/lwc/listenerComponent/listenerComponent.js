bd5318e13ef368c358302672656915d65b6f69c0
DunaevViktor <rulit.ivan@yandex.ru>
2020-11-11 16:50:54 +0300

    feat: add listenerComponent

---
+++ Task 1/force-app/main/default/lwc/listenerComponent/listenerComponent.js
@@ -0,0 +1,42 @@
+import { LightningElement } from 'lwc';
+import { subscribe, unsubscribe, onError } from 'lightning/empApi';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+export default class ListenerComponent extends LightningElement {
+    channelName = '/event/UpdateAccountPE__e';
+    subscription = {};
+
+    connectedCallback() {  
+        
+        console.log('2');
+
+        const messageCallback = (response) => {
+            const message = response['data']['payload']['Info__c'];
+            this.showToast('Account changes', message);
+        };
+        
+        subscribe(this.channelName, -1, messageCallback).then(response => {
+            this.subscription = response;
+        });
+
+        this.registerErrorListener();
+    }
+
+    registerErrorListener() {
+        onError(error => {
+            console.log('Received error from server: ', JSON.stringify(error));
+        });
+    }
+
+    showToast(title, message) {
+        const event = new ShowToastEvent({title, message});
+        this.dispatchEvent(event);
+    }
+
+    disconnectedCallback() {
+        unsubscribe(this.subscription, response => {
+            console.log('unsubscribe() response: ', JSON.stringify(response));
+        });
+    }
+
+}
--- Task 1/force-app/main/default/triggers/updateAccountTrigger.trigger
+++ Task 1/force-app/main/default/triggers/updateAccountTrigger.trigger
@@ -1,12 +1,4 @@
 trigger updateAccountTrigger on Account (after update) {
-
-    List<List<messageClass>> allAccMessages = new List<List<messageClass>>();
-
     TriggerHelper helper = new TriggerHelper();
-    allAccMessages = helper.findUpdatedFields(Trigger.oldMap, Trigger.newMap);
-    //System.debug('from tr: ' + allAccMessages);
-
-    //not bulkify!!!
-    helper.createEvent(allAccMessages[0]);
-
+    helper.findUpdatedFields(Trigger.oldMap, Trigger.newMap);
 }
