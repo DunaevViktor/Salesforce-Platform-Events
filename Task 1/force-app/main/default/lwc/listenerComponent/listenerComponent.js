13f63fee6f73d88ad4de8ca49994b681fc25b637
DunaevViktor <rulit.ivan@yandex.ru>
2020-11-14 21:13:21 +0300

    feat: add accountId check

--- Task 1/force-app/main/default/lwc/listenerComponent/listenerComponent.js
+++ Task 1/force-app/main/default/lwc/listenerComponent/listenerComponent.js
@@ -1,16 +1,20 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 import { subscribe, unsubscribe, onError } from 'lightning/empApi';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class ListenerComponent extends LightningElement {
     channelName = '/event/UpdateAccountPE__e';
     subscription = {};
+    @api recordId;
 
     connectedCallback() {  
 
         const messageCallback = (response) => {
             const message = response['data']['payload']['Info__c'];
-            this.showToast('Account changes', message);
+            var eventRecordId = response['data']['payload']['AccountId__c'];
+            if(eventRecordId == this.recordId){
+                this.showToast('Account changes', message);
+            }
         };
         
         subscribe(this.channelName, -1, messageCallback).then(response => {
